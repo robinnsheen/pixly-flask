4227d9e35109c376440dc0aa8ac3ce13d02d2ebc
robin <robinnsheen@berkeley.edu>
2022-07-26 15:47:51 -0700

    flask file function

--- app.py
+++ app.py
@@ -2,17 +2,21 @@ import os
 from dotenv import load_dotenv
 
 from flask import (
-    Flask, render_template, request, flash, redirect, session, g, abort,
+    Flask, url_for, request, flash, redirect, session, abort,
 )
+from werkzeug.utils import secure_filename
+
 
 from flask_debugtoolbar import DebugToolbarExtension
 
-# from forms import (
-#     PictureAddForm, PictureEditForm
-# )
-# from models import (
-#     db, connect_db, Picture, )
+from forms import (
+    PictureAddForm
+)
+from models import (
+    db, connect_db, Picture, )
 
+UPLOAD_FOLDER = './imgs'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 load_dotenv()
 
@@ -26,6 +30,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 toolbar = DebugToolbarExtension(app)
 
-# connect_db(app)
+connect_db(app)
--- models.py
+++ models.py
@@ -16,3 +16,13 @@ class Picture(db.Model):
         db.Integer,
         primary_key=True,
     )
+
+
+def connect_db(app):
+    """Connect this database to provided Flask app.
+
+    You should call this in your Flask app.
+    """
+
+    db.app = app
+    db.init_app(app)
